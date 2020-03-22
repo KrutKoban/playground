35f6f985b2230beeb2da40cb3b1eaf0e592d9732
Valeriy <vanikif7@mts.ru>
2020-03-22 15:50:08 +0300

    remove comments

--- src/store/modules/transactions/mutations.js
+++ src/store/modules/transactions/mutations.js
@@ -10,18 +10,9 @@ export default {
             const created = new Date(el.created_at);
             const year = created.getFullYear();
             let month = formatNumber(created.getMonth() + 1);
-            // if (String(month).length < 2) {
-            //     month = `0${month}`;
-            // }
             let date = formatNumber(created.getDate());
-            // if (String(date).length < 2) {
-            //     date = `0${date}`;
-            // }
             const hours = created.getUTCHours();
             let minutes = formatNumber(created.getUTCMinutes());
-            // if (String(minutes).length < 2) {
-            //     minutes = `0${date}`;
-            // }
             const formatDate = [year, month, date].join('-');
             const formatTime = [hours, minutes].join(':');
             return {
